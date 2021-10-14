9c1502f78fa9282f8741744f01536d1228f88e96
Md Rahmat <89685473+Itsmdrahmat@users.noreply.github.com>
2021-10-14 09:35:32 +0530

    Update Changepwd.php

--- Changepwd.php
+++ Changepwd.php
@@ -60,7 +60,7 @@ if(isset($_POST['submit']))
 <head>
 	<title>Create New Password</title>
 	<?php
-		include 'Style/css1.css';
+		include 'css1.css';
 	?>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
