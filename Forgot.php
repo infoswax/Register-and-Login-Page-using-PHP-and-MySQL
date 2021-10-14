8394a01d556a06f4b0dce9fbe46c3a2b96d7ec04
Md Rahmat <89685473+Itsmdrahmat@users.noreply.github.com>
2021-10-14 09:35:54 +0530

    Update Forgot.php

--- Forgot.php
+++ Forgot.php
@@ -55,7 +55,7 @@ session_start();
 <head>
 	<title>Forgot Password</title>
 	<?php
-		include 'Style/css1.css';
+		include 'css1.css';
 	?>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
